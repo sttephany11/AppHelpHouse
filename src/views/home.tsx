9c94aa1c6ae84dfede0b655ef3a85b2bb5096e19
KauaneBrandao <kauanesandes14@icloud.com>
2024-08-27 23:34:50 -0300

    iniciei o front da tela home

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,116 +1,108 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity,TextInput, SafeAreaView, ScrollView, ImageBackground, StatusBar, Image} from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, TextInput, ScrollView, Image, ImageBackground } from 'react-native';
+import { FloatingLabelInput } from 'react-native-floating-label-input';
+import { TextInputMask } from 'react-native-masked-text';
+import { Button } from "../../componentes/Button/Button";
 import Imagens from "../../img/img";
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+
 import styles from '../css/homeCss';
-import pesquisar from "../views/pesquisar";
+const AreaAtuacao: React.FC<{ navigation: any }> = ({ navigation }) => {
 
-const Home: React.FC<{ navigation: any }> = ({ navigation }) => {
-    
-    
     return (
-        <ImageBackground 
-        source={Imagens.fundoBemVindo}
-        style={styles.background}  // Define o estilo para a imagem de fundo
-        resizeMode="cover"   // Ajusta a imagem para cobrir a tela 
+        <ImageBackground
+            source={Imagens.fundoBemVindo}
+            style={styles.background}  // Define o estilo para a imagem de fundo
+            resizeMode="cover"   // Ajusta a imagem para cobrir a tela
         >
-       <View style={styles.container}>
-        <ScrollView>
 
- 
-        <View style={styles.container}>
-        
-        <View style={styles.container2}>
+            {/* <View style={styles.container}></View>
+<ScrollView></ScrollView> */}
 
-                <View style={styles.posicao}>
-                <TouchableOpacity onPress={() => navigation.navigate('Pesquisar')}>
-                    <View style={styles.coluna1}>
-                        <Text style={styles.texto1}>Qual serviço você procura?</Text>
-                        <Image style={styles.lupaGrande} source={Imagens.lupaGrande} />
-                    </View>
-                    </TouchableOpacity >
+            <View style={styles.container2}>
+                <Text style={styles.boasVindas}>Boas-vindas!</Text>
+
+                <View style={styles.circulo}></View>
+
+                <View style={styles.pesquisa}>
+                    <Image source={Imagens.lupaAzul} style={styles.lupa} />
+                    <TextInput
+                        placeholder='O que você precisa?'
+                        style={styles.pesquisa1}
+                        placeholderTextColor='#004aad'
+                        keyboardType='default'
+                    />
+
                 </View>
-           
-        </View>
-
-        <View style={styles.container2}>
-            <View style={styles.propaganda}>
-                <Text style={styles.titulo}>Serviços</Text>
-                <Text style={styles.subtitulo}>Encontre aqui algumas áreas mais procuradas.</Text>
             </View>
-        </View>
-
-        <View style={styles.container2}>
-            <TouchableOpacity>
-                    <View style={styles.colunaOpcao}>
-                        <Image style={styles.encanadorImg} source={Imagens.encanador} />
-                        <Text style={styles.textoOpcao}>Encanamento</Text>
-                    </View>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.container2}>
-            <TouchableOpacity>
-                    <View style={styles.colunaOpcao2}>
-                        <Image style={styles.encanadorImg} source={Imagens.eletrica} />
-                        <Text style={styles.textoOpcao}>Elétrica</Text>
-                    </View>
-            </TouchableOpacity>
-        </View>
-
-        <View style={styles.container2}>
-            <TouchableOpacity>
-                    <View style={styles.colunaOpcao2}>
-                        <Image style={styles.encanadorImg} source={Imagens.capacete} />
-                        <Text style={styles.textoOption}>Construção e reparos</Text>
-                    </View>
-            </TouchableOpacity>
-        </View>
-
-        <View style={styles.container2}>
-           <View style={styles.colunaServico}>
-               <Text style={styles.texto2}>Alguns dos serviços mais procurados ultimamente</Text>
+            
+            <View style={styles.brancoMeio}>
+                <Text style={styles.pedidos}> Meus pedidos</Text>
+                <Text style={styles.acompanhe}> Acompanhe seus pedidos...</Text>
            
-           <View style={styles.servicosProcurado}>
-           <TouchableOpacity>
-               <Image style={styles.chuveiroImg} source={Imagens.chuveiro} />
-           </TouchableOpacity>
+            </View>
 
-           <TouchableOpacity>
-               <Image style={styles.chuveiroImg} source={Imagens.kitEletrico} />
-           </TouchableOpacity>
 
-           <TouchableOpacity>
-                <Image style={styles.chuveiroImg} source={Imagens.manutencao} />
-           </TouchableOpacity>
+            <View style={styles.trabalhos}>
 
-           </View>
-           </View>
-        </View>
-        
-         </View>
+                <View style={styles.pedreiro}>
+                    <Text style={styles.name1}>Pedreiro</Text>
+                    <Image source={Imagens.pedreiro} style={styles.pedreiroImg} />
+                </View>
 
-        </ScrollView>
-        </View>
+                <View style={styles.mecanico}>
+                    <Text style={styles.name2}>Mecânico</Text>
+                    <Image source={Imagens.mecanico} style={styles.mecanicoImg} />
+                </View>
+
+                <View style={styles.eletricista}>
+                    <Text style={styles.name3}>Eletricista</Text>
+                    <Image source={Imagens.eletricista} style={styles.eletricistaImg} />
+                </View>
+
+                <View style={styles.jadineiro}>
+                    <Text style={styles.name4}>Jadineiro</Text>
+                    <Image source={Imagens.jardineiro} style={styles.jardineiroImg} />
+                </View>
+
+
+                <View style={styles.encanador}>
+                    <Text style={styles.name5}>Encanador</Text>
+                    <Image source={Imagens.encanadorK} style={styles.encanadorImg} />
+                </View>
+
+                <View style={styles.diarista}>
+                    <Text style={styles.name6}>Serviço</Text>
+                    <Text style={styles.domestico}>doméstico</Text>
+                    <Image source={Imagens.diarista} style={styles.diaristaImg} />
+                </View>
+
+                <View style={styles.pintor}>
+                    <Text style={styles.name7}>Pintor</Text>
+                    <Image source={Imagens.pintor} style={styles.pintorImg} />
+                </View>
+
+                <View style={styles.instalacao}>
+                    <Text style={styles.name8}>Instalação</Text>
+                    <Image source={Imagens.instalacao} style={styles.instalacaoImg} />
+                </View>
+
+            </View>
+
+            <View style={styles.brancoMeio}>
+                <Text style={styles.pedidos}> Meus pedidos</Text>
+                <Text style={styles.acompanhe}> Acompanhe seus pedidos...</Text>
+            </View>
 
         </ImageBackground>
     );
 };
 
-/*
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="pesquisar" component={pesquisar} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-*/
+export default AreaAtuacao;
 
 
 
-export default Home;
+
+
+

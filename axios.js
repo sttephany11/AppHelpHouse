55ca2ea854fe00e7a3119739e4253fac7cbf7113
sttephany11 <rodriguesstephany76@gmail.com>
2024-10-23 20:43:41 -0300

    marge

--- axios.js
+++ axios.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-    baseURL: 'http://172.20.10.14:8000/api',
+    baseURL: 'http://192.168.1.13:8000/api',
 });
 
 export default axios.create({
-    baseURL:'http://172.20.10.14:8000/api'
+    baseURL:'http://192.168.1.13:8000/api'
 })

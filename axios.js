35a17783e742f5cba5a786b1912d5f24e72145f9
KauaneBrandao <kauanesandes14@icloud.com>
2024-11-25 08:59:31 -0300

    fiz as bolinhas de marcação

--- axios.js
+++ axios.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
 
-    baseURL:'http://192.168.1.10:8000/api',
+    baseURL:'http://172.20.10.2:8000/api',
    
 })
 

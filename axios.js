5a352e649ed755e1d67bfb26a39695b8c5cfd508
Guilherme Mesquita <guimesquita.1512@gmail.com>
2024-11-25 22:29:19 -0300

    aaaa

--- axios.js
+++ axios.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
 
-    baseURL:'http://172.20.10.2:8000/api',
+    baseURL:'http://10.0.0.121:8000/api',
    
 })
 
